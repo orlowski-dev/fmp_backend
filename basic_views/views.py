ff9c0ad975629c9a467d2938650d6b67fdf440c2
Patryk Orłowski <orlowski.dev@gmail.com>
2024-09-24 09:38:41 +0200

    Updated: home-page

--- basic_views/views.py
+++ basic_views/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from oauth2_provider.models import AccessToken
 
 
 @login_required()
 def home_view(request):
-    return render(request, "basic_views/home-page.html")
+    tokens = None
+    if request.user.is_superuser:
+        tokens = AccessToken.objects.all()
+    else:
+        tokens = AccessToken.objects.all().filter(user=request.user)
+    return render(request, "basic_views/home-page.html", {"tokens": tokens})
